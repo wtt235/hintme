b853e86cbe2cd97263611da9598ad7e6134a8eeb
todd <williamtoddt@msn.com>
2014-11-12 22:53:23 -0600

    added express

---
+++ src/server.js
@@ -0,0 +1,36 @@
+var r = require('rethinkdb'),
+    express = require('express');
+var rdb_conn;
+r.connect({host: 'localhost',
+           port: 28015,
+           db: 'hmbomt'}, 
+          function(err, conn){
+            if (err) throw err;
+            rdb_conn = conn;
+          });
+
+var app = express();
+app.static('../public/');
+
+app.get('/', function(req, res){
+  res.send("Hello, World");
+});
+
+app.get('/api/v0/users', function(req, res){
+  r
+    .db('hmbomt')
+    .table('users')
+    .run(rdb_conn, function(err, cursor){
+      if (err) throw err;
+      cursor.toArray(function(err, arr){
+        res.json(arr);
+      });
+    });
+});
+        
+var server = app.listen(9000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Server has started at host " + host + ":" + port);
+});
